ff37b857110f941876ede47ca19be2573b5a84e1
Jackson Dorsett <dorsettj@gmail.com>
2024-07-31 10:37:23 -0700

    added description and meta section

--- src/Components/MangaTitleInfo/MangaTitleInfo.tsx
+++ src/Components/MangaTitleInfo/MangaTitleInfo.tsx
@@ -3,14 +3,29 @@ import { SeriesInfo } from '../../utils/getSeriesInfoHook';
 import TitleCoverBackground from './TitleCoverBackground';
 import MangaHeaderImage from './MangaHeaderImage';
 import MangaHeaderBox from './MangaHeaderBox';
+import MangaDescriptionBox from './MangaDescriptionBox';
+import MetaSectionBox from './MetaSectionBox';
 const MangaTitleInfo = (props: SeriesInfo) => {
   return (
-    <div className="manga-title-info relative text-white flex">
+    <div className="manga-title-info relative text-white flex flex-col">
       <TitleCoverBackground cover={props.image} title={props.title} />
       <div className="series-header-container flex flex-row w-full ">
         <MangaHeaderImage image={props.image} />
 
         <MangaHeaderBox title={props.title} author={props.author} />
+      </div>
+
+      <MangaDescriptionBox description={props.description} />
+
+      <div className="content-section flex flex-row justify-between p-4">
+        <div className="series-meta-info w-1/2">
+          <MetaSectionBox
+            author={props.author}
+            genres={props.genres}
+            status={props.status}
+          />
+        </div>
+        <div className="series-chapters w-full">CHAPTERS</div>
       </div>
     </div>
   );
