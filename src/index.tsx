e4493d47ee9c07753273788db1d0aead8d9265d5
Jackson Dorsett <dorsettj@gmail.com>
2024-08-04 22:00:14 -0700

    add font import

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/globals.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import './styles/globals.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const container = document.getElementById('root');
 
