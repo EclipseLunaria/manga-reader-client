96d8e5d90e629dd1d558267fa9fa9b928c1bc910
Jackson Dorsett <dorsettj@gmail.com>
2024-08-08 15:25:38 -0700

    fix page

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,21 @@
-const env =
-  process.env.NODE_ENV === 'production' ? 'production' : 'development';
-console.log('env:', process.env.NODE_ENV);
-const API_BASE_URL =
-  env === 'production'
-    ? 'https://manga.eclipselunaria.dev/api'
-    : 'http://localhost:6900';
+// Load environment variables from .env file if in development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
+// Log the current environment
+console.log('Environment:', process.env.NODE_ENV);
+
+// Define the base URLs for different environments
+const config = {
+  production: 'https://manga.eclipselunaria.dev/api',
+  development: 'http://localhost:6900',
+  // Optionally, add more environments like staging, testing, etc.
+};
+
+// Select the API base URL based on the current environment
+const API_BASE_URL =
+  config[process.env.NODE_ENV as keyof typeof config] || config.development;
+
+// Export the API base URL for use in other modules
 export { API_BASE_URL };
--- src/utils/hooks/getSeriesInfoHook.tsx
+++ src/utils/hooks/getSeriesInfoHook.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
 import { SeriesInfo } from '../types';
-import path from 'path';
 import { API_BASE_URL } from '../api';
 
 const getSeriesInfoHook = (mangaId: string) => {
   const [seriesInfo, setSeriesInfo] = useState<SeriesInfo | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-
+  console.log(API_BASE_URL);
   useEffect(() => {
     const fetchSeriesInfo = async () => {
       const ENDPOINT_URL = `${API_BASE_URL}/manga/${mangaId}`;
