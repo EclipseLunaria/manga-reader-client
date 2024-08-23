26be1839a959e301a999664bfcfa9a7280e8e504
Jackson Dorsett <dorsettj@gmail.com>
2024-08-23 10:43:16 -0700

    remove logged out

--- src/Components/UserInfo/UserMenu.tsx
+++ src/Components/UserInfo/UserMenu.tsx
@@ -1,12 +1,8 @@
 import React, { ReactNode, useEffect, useState } from 'react';
-import { IUserInfo } from '../../interfaces/bearer.interface';
-import LoggedInUserMenu from './LoggedInUserMenu';
 import LoggedOutMenu from './LoggedOutMenu';
 
 const UserMenu = (props: { closeMenu: () => void }) => {
   const [activeModal, setActiveModal] = useState<null | ReactNode>(null);
-  const userInfo: IUserInfo =
-    JSON.parse(localStorage.getItem('user') || '') ?? null;
 
   const onModalClose = () => {
     setActiveModal(null);
@@ -15,10 +11,7 @@ const UserMenu = (props: { closeMenu: () => void }) => {
   useEffect(() => {}, [activeModal]);
   return (
     <div className="menu absolute w-[300px] h-[500px] bg-primary rounded-lg right-0 mt-2 p-4 z-20">
-      {userInfo
-        ? LoggedInUserMenu(userInfo)
-        : LoggedOutMenu({ setActiveModal, onModalClose })}
-      {activeModal}
+      {LoggedOutMenu({ setActiveModal, onModalClose })}
     </div>
   );
 };
